83bb735ac89115b223b985e66569151cd88299a3
AndyXue1019 <76648359+AndyXue1019@users.noreply.github.com>
2025-04-22 18:16:52 +0800

    Add files via upload

---
+++ ros2_jazzy_install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+echo ""
+echo "[Note] OS version  >>> Ubuntu 24.04 (Noble Numbat)"
+echo "[Note] Target ROS version >>> ROS 2 Jazzy Jalisco"
+echo "[Note] Colcon workspace   >>> $HOME/colcon_ws"
+echo ""
+echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
+echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
+read
+
+echo "[Set the target ROS version and name of colcon workspace]"
+name_ros_version=${name_ros_version:="jazzy"}
+name_colcon_workspace=${name_colcon_workspace:="colcon_ws"}
+
+echo "[Set Locale]"
+sudo apt update && sudo apt install -y locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+echo "[Setup Sources]"
+sudo rm -rf /var/lib/apt/lists/* && sudo apt update && sudo apt install -y curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null'
+
+echo "[Install ROS 2 packages]"
+sudo apt update && sudo apt install -y ros-$name_ros_version-desktop \
+  ros-$name_ros_version-joy ros-$name_ros_version-teleop-twist-joy \
+  ros-$name_ros_version-teleop-twist-keyboard ros-$name_ros_version-laser-proc \
+  ros-$name_ros_version-urdf ros-$name_ros_version-xacro ros-$name_ros_version-rqt* \
+  ros-$name_ros_version-compressed-image-transport ros-$name_ros_version-rviz2 \
+  ros-$name_ros_version-navigation2 ros-$name_ros_version-slam-toolbox \
+  ros-$name_ros_version-interactive-markers ros-$name_ros_version-dynamixel-sdk \
+  ros-$name_ros_version-cartographer ros-$name_ros_version-cartographer-ros \
+  ros-$name_ros_version-nav2-bringup ros-$name_ros_version-ros-gz 
+
+echo "[Environment setup]"
+source /opt/ros/$name_ros_version/setup.sh
+sudo apt install -y python3-argcomplete python3-colcon-common-extensions python3-vcstool python3-rosdep git
+
+# echo "[Install dependencies]"
+# sudo rosdep init && rosdep update
+# cd $HOME/$name_colcon_workspace
+# rosdep install -y --from-paths src --ignore-src --rosdistro $name_ros_version
+
+echo "[Make the colcon workspace and test colcon build]"
+mkdir -p $HOME/$name_colcon_workspace/src
+cd $HOME/$name_colcon_workspace/src
+cd $HOME/$name_colcon_workspace
+colcon build --symlink-install
+
+echo "[Set the ROS evironment]"
+sh -c "echo \"\" >> ~/.bashrc"
+sh -c "echo \"alias eb='vim ~/.bashrc'\" >> ~/.bashrc"
+sh -c "echo \"alias nb='nano ~/.bashrc'\" >> ~/.bashrc"
+sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
+sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
+sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
+
+sh -c "echo \"\" >> ~/.bashrc"
+sh -c "echo \"alias cw='cd ~/$name_colcon_workspace'\" >> ~/.bashrc"
+sh -c "echo \"alias cs='cd ~/$name_colcon_workspace/src'\" >> ~/.bashrc"
+sh -c "echo \"alias cb='cd ~/$name_colcon_workspace && colcon build --symlink-install && source ~/.bashrc'\" >> ~/.bashrc"
+
+sh -c "echo \"\" >> ~/.bashrc"
+sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
+sh -c "echo \"source ~/$name_colcon_workspace/install/local_setup.bash\" >> ~/.bashrc"
+sh -c "echo \"source ~/$name_colcon_workspace/install/setup.bash\" >> ~/.bashrc"
+
+sh -c "echo \"export ROS_DOMAIN_ID=30 # 0~101\" >> ~/.bashrc"
+
+source ~/.bashrc
+
+echo "[Complete!!!]"
+
+exec bash
+exit 0
